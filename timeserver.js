6a40ec9daa982e26694ecd8475b05be34add62ea
elsehow <indra@web482.webfaction.com>
2015-05-07 06:25:51 +0000

    changed timeserver to restify

--- timeserver.js
+++ timeserver.js
@@ -1,14 +1,23 @@
-app = require('express')()
+var restify = require('restify')
+
+port = 16649
+
+var server = restify.createServer({});
 
 // allow CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+server.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+)
 
 // handle get requests
-app.get('/', function (req, res) { res.json(new Date())})
+server.get('/', function (req, res, next) { 
+  res.send(200, new Date())
+})
 
 // run server
-server = app.listen(16649)
+server.listen(port)
+console.log('listening on port ' + port)
