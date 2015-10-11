e78d766e6dd615790ade6342f6a06d0e1590907e
elsehow <indra@web482.webfaction.com>
2015-10-11 07:39:58 +0000

    changed response headers to text/plain

--- timeserver.js
+++ timeserver.js
@@ -7,15 +7,16 @@ var server = restify.createServer({});
 // allow CORS
 server.use(
   function crossOrigin(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    res.header("Content-Type", "text/plain")
     return next();
   }
 )
 
 // handle get requests
 server.get('/', function (req, res, next) { 
-  res.send(200, new Date())
+  res.send(200, new Date().toISOString())
 })
 
 // run server
