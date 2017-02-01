6d4537b11e78fe51c9b5774458fec22c04fbcb2a
elsehow <yes@cosmopol.is>
2017-02-01 15:25:01 -0800

    refactor

--- timeserver.js
+++ timeserver.js
@@ -1,27 +1,10 @@
 #!/usr/bin/env node
 
-var restify = require('restify')
-  , argv = require('minimist')(process.argv.slice(2))
-  , port = parseInt(argv._)
-  , server = restify.createServer({})
+var argv = require('minimist')(process.argv.slice(2))
+, port = parseInt(argv._)
+, timeserver = require('./index.js')
 
 if (!port) { port = 8888 }
-
-// allow CORS
-server.use(
-  function crossOrigin(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "X-Requested-With")
-    res.header("Content-Type", "text/plain")
-    return next()
-  }
-)
-
-// handle get requests
-server.get('/', function (req, res, next) { 
-  res.send(200, new Date().toISOString())
-})
-
-// run server
+var server = timeserver()
 server.listen(port)
-console.log('listening on 0.0.0.0:' + port)
+console.log('timeserver listening on localhost:'+port)
